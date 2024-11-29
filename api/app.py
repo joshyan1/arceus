2a047af625f5bff3168bf4b8f085fe338be97e69
Ishaan Dey <ishaan1013@gmail.com>
2024-11-29 15:56:48 -0500

    3000 and 8000

--- api/app.py
+++ api/app.py
@@ -6,11 +6,16 @@ from flask_socketio import SocketIO
 # Initialize Flask app
 app = Flask(__name__)
 
-# Allow CORS from http://127.0.0.1:8000
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+# Allow CORS from both ports
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000",
+     "http://localhost:8000", "http://127.0.0.1:3000", "http://127.0.0.1:8000"]}})
 
-# Initialize Socket.IO with the app
+# Initialize Socket.IO with multiple allowed origins
 socketio = SocketIO(
-    app, cors_allowed_origins="http://localhost:3000", async_mode='threading')
+    app,
+    cors_allowed_origins=["http://localhost:3000", "http://localhost:8000",
+                          "http://127.0.0.1:3000", "http://127.0.0.1:8000"],
+    async_mode='threading'
+)
 
 print('SocketIO async_mode is', socketio.async_mode)
